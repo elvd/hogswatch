5e53da7277ea8a4c280775a182e27e6efd5f6ba6
Viktor Doychinov <v.o.doychinov@leeds.ac.uk>
2015-11-12 00:20:21 +0000

    Hogswatch - Full operational functionality ==========================================
    Wrote a nice and long commit message, that got lost when trying to push
    from within Atom. Will re-write tomorrow, as I am quite tired right now.

--- hogswatch.ino
+++ hogswatch.ino
@@ -1,18 +1,25 @@
 /*
-** Hog's watch - Hedgehog vivarium monitor
-** 
-** Uses a rare-earth magnet and a hall effect sensor to detect
-** and count wheel spins, and estimate total distance run.
-** 
-** Reads temperature data from a DHT22 sensor.
-** 
-** Sends data to a Raspberry Pi over serial port connection.
-** 
+** Hogswatch - African Pygmy Hedgehog vivarium monitor
+** ===================================================
+**
+** Continuously monitors vivarium temperature, humidity, and wheel activity for
+** two African Pygmy Hedgehogs, Caesar and Tzaritza (Ritz).
+**
+** A DHT 22 sensor provides temperature and humidity data. Adafruit's library
+** is used for communication with the sensors.
+**
+** A combination of magnets, attached to the hedgehogs' wheels and Hall sensors
+** are used to detect wheel rotation and count number of revolutions.
+**
+** Data is transmitted through a serial link to a Raspberry Pi for further
+** processing, logging, and uploading to the Internet.
+**
+** Author: Viktor Doychinov (v.o.doychinov@gmail.com)
 **
 */
 
 #include <LiquidCrystal.h> // for debugging and testing purposes
-// #include <SoftwareSerial.h> // Xbee communication, using dumb serial mode
+#include <SoftwareSerial.h> // RPi communication, using dumb serial mode
 #include "DHT.h" // Adafruit library for DHT22 sensor
 
 // LCD pins, with corresponding pins on LCD board
@@ -32,28 +39,33 @@
 #define RITZHALL2 3
 
 // Raspberry Pi communication serial pins
-// #define PIRX 4
-// #define PITX 5
+#define PIRX A1
+#define PITX A0
 
-// DHT sensor pins
+// DHT sensor configuration
 #define DHTTYPE DHT22
 #define CDHTPIN A3
 #define RDHTPIN A5
 
-// Raspberry Pi TX periods
+// Raspberry Pi TX time intervals
 #define TXREVS 60000
 #define TXTEMP 60000
-#define RESETPERIOD 86400000
-#define UPDATEPERIOD 60000
 
+// Reset number of revolutions every day
+#define RESETPERIOD 86400000
+
+// Update LCD display every second.
+#define UPDATEPERIOD 1000
+
+// for detecting direction and number of revolutions
+int c_flag1 = 0, c_flag2 = 0, r_flag1 = 0, r_flag2 = 0;
 int hall_value1, hall_value2;
-int c_flag1 = 0, c_flag2 = 0, r_flag1 = 0, r_flag2 = 0; // for detecting direction and number of revolutions
 unsigned long c_revolutions = 0, r_revolutions = 0;
 unsigned long timer_revs = 0, timer_temp = 0, timer_start = 0, timer_update = 0;
 float temp = 0.0, humidity = 0.0;
 
 // initialise the separate components/libraries
-// SoftwareSerial pi_port(PIRX, PITX) ;
+SoftwareSerial pi_port(PIRX, PITX) ;
 LiquidCrystal stat_disp(RS, E, D4, D5, D6, D7);
 DHT c_temp_sensor(CDHTPIN, DHTTYPE);
 DHT r_temp_sensor(RDHTPIN, DHTTYPE);
@@ -63,21 +75,21 @@ void setup() {
   pinMode(CAESARHALL2, INPUT);
   pinMode(RITZHALL1, INPUT);
   pinMode(RITZHALL2, INPUT);
-  
-//  Pi_port.begin(9600);
-  Serial.begin(9600);
+
+  pi_port.begin(9600);
+//  Serial.begin(9600);
   c_temp_sensor.begin();
   r_temp_sensor.begin();
-   
+
   stat_disp.begin(COLS, ROWS);
-  stat_disp.print("Hello Kate!");
+  stat_disp.print("Hello Kate!"); // My lovely girlfriend and best hog mummy
   stat_disp.setCursor(0, 1);
   stat_disp.print("Huff Huff");
-  
+
   delay(5000);
   stat_disp.clear();
-  
-// Get current time  
+
+// Get current time
   timer_temp = millis();
   timer_revs = millis();
   timer_start = millis();
@@ -86,84 +98,93 @@ void setup() {
 
 void loop() {
 
-  if ((millis() - timer_revs) >= TXREVS) {
-//    Pi_port.print("caesar:dist:");
-//    Pi_port.print(c_revolutions);
-//    Pi_port.println();
+  if ((millis() - timer_revs) >= TXREVS) { // send number of revolutions to RPi
+    pi_port.print("caesar:dist:");
+    pi_port.println(c_revolutions);
 
-//    Pi_port.print("ritz:dist:");
-//    Pi_port.print(r_revolutions);
-//    Pi_port.println();
+    pi_port.print("ritz:dist:");
+    pi_port.println(r_revolutions);
 
-    Serial.print("caesar:dist:");
-    Serial.println(c_revolutions);
-
-    Serial.print("ritz:dist:");
-    Serial.print(r_revolutions);
+//    Serial.print("caesar:dist:");
+//    Serial.println(c_revolutions);
+//
+//    Serial.print("ritz:dist:");
+//    Serial.println(r_revolutions);
 
     timer_revs = millis();
   }
-  
-  if ((millis() - timer_temp) >= TXTEMP) {
+
+  if ((millis() - timer_temp) >= TXTEMP) { // send temp and humidity to RPi
     temp = c_temp_sensor.readTemperature();
     humidity = c_temp_sensor.readHumidity();
-//    Pi_port.print("caesar:temp:");
-//    Pi_port.print(temp);
-//    Pi_port.println();
-    Serial.print("caesar:temp:");
-    Serial.println(temp);
-    Serial.print("caesar:humidity:");
-    Serial.println(humidity);
+
+    pi_port.print("caesar:temp:");
+    pi_port.println(temp);
+    pi_port.print("caesar:humidity:");
+    pi_port.println(humidity);
+
+//    Serial.print("caesar:temp:");
+//    Serial.println(temp);
+//    Serial.print("caesar:humidity:");
+//    Serial.println(humidity);
 
     temp = r_temp_sensor.readTemperature();
     humidity = r_temp_sensor.readHumidity();
-//    Pi_port.print("ritz:temp:");
-//    Pi_port.print(temp);
-//    Pi_port.println();
-    Serial.print("ritz:temp:");
-    Serial.println(temp);
-    Serial.print("ritz:humidity:");
-    Serial.println(humidity);
+
+    pi_port.print("ritz:temp:");
+    pi_port.println(temp);
+    pi_port.print("ritz:humidity:");
+    pi_port.println(humidity);
+
+//    Serial.print("ritz:temp:");
+//    Serial.println(temp);
+//    Serial.print("ritz:humidity:");
+//    Serial.println(humidity);
 
     timer_temp = millis();
   }
-  
+
   if ((millis() - timer_start) >= RESETPERIOD) {
     c_revolutions = 0;
     r_revolutions = 0;
     timer_start = millis();
   }
-  
+
   if ((millis() - timer_update) >= UPDATEPERIOD) {
     update_display();
     timer_update = millis();
   }
-  
+
   check_caesar();
-  Serial.print("c_flag1:");
-  Serial.println(c_flag1);
-  Serial.print("c_flag2:");
-  Serial.println(c_flag2);
   check_ritz();
-  Serial.print("r_flag1:");
-  Serial.println(r_flag1);
-  Serial.print("r_flag2:");
-  Serial.println(r_flag2);
 }
 
 void update_display() {
   stat_disp.clear();
-  
+
   stat_disp.setCursor(0, 0);
   stat_disp.print("CSR REV:");
   stat_disp.print(c_revolutions);
 
   stat_disp.setCursor(0, 1);
-  stat_disp.print("RTZ REV:");
+  stat_disp.print("TZA REV:");
   stat_disp.print(r_revolutions);
 }
 
-void check_caesar() { 
+/*
+** Both check_ritz() and check_caesar() use the same algorithm, just different
+** sensors. If any Hall sensor detectes LOW, i.e. magnetic field is present, a
+** flag is set. If then the next Hall sensor also registers a magnetic field,
+** the number of wheel rotations for the specific hedgehog is increased by one.
+** Assumption is that said magnetic field is caused by the magnets attached to
+** their wheels.
+**
+** For more information, look at accompanying sketch.
+**
+**
+*/
+
+void check_caesar() {
   hall_value1 = digitalRead(CAESARHALL1);
   hall_value2 = digitalRead(CAESARHALL2);
 
@@ -173,9 +194,9 @@ void check_caesar() {
       c_revolutions = c_revolutions + 1;
       c_flag2 = 0; }
     else if (c_flag1 == 1) c_flag1 = 0;
-    delay(250);
+    delay(250); // TODO: use a different debounce method
   }
-  
+
   if (hall_value2 == LOW) {
     if (c_flag1 == 0 && c_flag2 == 0) c_flag2 = 1;
     else if (c_flag1 == 1) {
@@ -196,9 +217,9 @@ void check_ritz() {
       r_revolutions = r_revolutions + 1;
       r_flag2 = 0; }
     else if (r_flag1 == 1) r_flag1 = 0;
-    delay(250);
+    delay(250); // TODO: use a different debounce method
   }
-  
+
   if (hall_value2 == LOW) {
     if (r_flag1 == 0 && r_flag2 == 0) r_flag2 = 1;
     else if (r_flag1 == 1) {
@@ -206,6 +227,5 @@ void check_ritz() {
       r_flag1 = 0; }
     else if (r_flag2 == 1) r_flag2 = 0;
     delay(250);
-  }  
+  }
 }
-
--- hogswatch.py
+++ hogswatch.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python
+#
+# Hogswatch - African Pygmy Hedgehog vivarium monitor
+# ===================================================
+#
+# Python script, meant to run on a Raspberry Pi and complement the Arduino
+# sketch collecting temperature, humidity, and wheel activity information.
+#
+# Logs all received data to a csv file, and sends text warnings to mobile phone
+# in case of a low or high temperature in either vivarium. Also calculates
+# distance ran from number of revolutions received.
+#
+# Communication with the Arduino is over serial link, using a SparkFun level
+# converter to go between 5 V (Arduino) and 3.3 V (RPi) logic.
+#
+# TODO: Add function to push data to Xively.
+# TODO: Move to `requests' instead of `urllib'
+# TODO: Move usernames/passwords/API to JSON file
+# TODO: Use `logging' module for service messages
+#
+# Author: Viktor Doychinov (v.o.doychinov@gmail.com)
+#
+
 
 import serial
 import datetime
 import csv
 import urllib
+from collections import namedtuple
 
-WHEEL_DIST = 0.94248  # circumference of a typical bucket wheel
-TEMP_MIN = 21
-TEMP_MAX = 27
-BAUD_RATE = 9600
-PORT_NAME = '/dev/ttyACM0'
-LOG_FILE = 'hogswatch.csv'
+wheel_dist = 0.94248  # circumference of a typical bucket wheel
 
+# as recommended by African Pygmy Hedgehog breeders and keepers
+temp_min = 21
+temp_max = 27
+
+baud_rate = 9600
+arduino_port = '/dev/ttyAMA0'
+logfile_name = 'hogswatch.csv'
+url = 'http://www.bulksms.co.uk:5567/eapi/submission/send_sms/2/2.0'
 input_port = None
-dist_sum = 0
-ts_general = None
-ts0 = datetime.datetime.now()
-ts1 = None
-flag_tweeted = 0
-flag_tweeted_dist = 0
+timestamp = None
+data = namedtuple('data', 'hedgehog param value')  # for better readability
+
+print('Variables defined successfully')
 
 
 def send_text(input_data):
-    url = "http://www.bulksms.co.uk:5567/eapi/submission/send_sms/2/2.0"
-
     warning_msg = ' '.join(['Temperature out of bounds in ',
-                            input_data[0].upper(),
+                            input_data.hedgehog.upper(),
                             '\'s viv! Temperature is:',
-                            str(input_data[2])])
+                            str(input_data.value)])
 
     param = urllib.urlencode({'username': 'toolverine',
                               'password': 'hedgehog',
@@ -37,68 +59,62 @@ def send_text(input_data):
     f = urllib.urlopen(url, param)
     s = f.read()
     result = s.split('|')
-    statusCode = result[0]
-    statusString = result[1]
+    status_code = result[0]
+    status_string = result[1]
     f.close()
 
-    return statusCode, statusString
+    return status_code, status_string
 
 
 def is_unsuitable(temp):
-    result = temp < TEMP_MIN or temp > TEMP_MAX
-    return result
+    return temp < temp_min or temp > temp_max
 
 
 try:
-    input_port = serial.Serial(PORT_NAME, BAUD_RATE)
+    input_port = serial.Serial(arduino_port, baud_rate)
     input_port.open()
 
-    log_file = open(LOG_FILE, 'a')
-    lf_writer = csv.writer(log_file)
+    logfile = open(logfile_name, 'a')
+    lf_writer = csv.writer(logfile)
 
 except:
-    print 'Error during initialisation'
+    print('Error during initialisation')
     raise SystemExit
+
+print('Serial port opened successfully')
+
+# in case of RPi reboot, Arduino might have sent data previously - discard it
+input_port.flushInput()
 
 while True:
     try:
         input_data = input_port.readline()
+        input_data = input_data.strip()
         input_data = input_data.split(':')
+        input_data = data(*input_data)
 
-        ts_general = datetime.datetime.now()
+        timestamp = datetime.datetime.now()
 
-        if input_data[1] == 'Temp' and is_unsuitable(float(input_data[2])):
-            sc, ss = send_text(input_data)
-            if sc != '0':
-                log_msg = str(ts_general), ss
-            else:
-                log_msg = str(ts_general), 'Temp warning msg sent'
+        if input_data.param == 'temp' and \
+           is_unsuitable(float(input_data.value)):  # TODO: add a timer
+            # sc, ss = send_text(input_data)
+            # if sc != '0':
+            #     log_msg = str(timestamp), ss
+            # else:
+            #     log_msg = str(timestamp), 'Temperature warning message sent'
+            log_msg = [timestamp, 'Temperature warning']
             lf_writer.writerow(log_msg)
 
-        if input_data[1] == 'Dist' and input_data[2] != 0:
-            dist_sum += input_data[2]
-            if flag_tweeted == 1:
-                pass
-            else:
-                ts1 = datetime.datetime.now()
+        if input_data.param == 'dist':
+            actual_dist = float(input_data.value) * wheel_dist
+            input_data = input_data._replace(value=actual_dist)
 
-        if input_data[1] == 'Dist' and input_data[2] == 0:
-            if flag_tweeted == 1:
-                ts0 = datetime.datetime.now()
-                flag_tweeted = 0
-
-        time_hr = ts_general.hour
-
-        if time_hr == 9 and flag_tweeted_dist == 0:
-            dist_sum = 0
-            flag_tweeted_dist = 1
-        elif time_hr != 9 and flag_tweeted_dist == 1:
-            flag_tweeted_dist = 0
-
-        lf_writer.writerow([ts_general, input_data])
+        lf_writer.writerow([timestamp, input_data.hedgehog,
+                            input_data.param, input_data.value])
+        logfile.flush()
 
     except:
         pass
 
 input_port.close()
-log_file.close()
+logfile.close()
